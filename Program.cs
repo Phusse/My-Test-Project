6df881f67758671e4a9117fd91083c3162c97a18
Dubem <sasuk3san@gmail,com>
2022-04-11 12:46:54 +0100

    On branch Master  Changes to be committed:

--- Program.cs
+++ Program.cs
@@ -1,14 +1,14 @@
 ﻿{
-     int num1= 0; int num2= 0;
+     int Digit1= 0; int Digit2= 0;
 
      Console.WriteLine("Console calculator built with C#");
      Console.WriteLine("--------------------------------");
 
      Console.WriteLine("Type a number, then press Enter");
-    num1 = Convert.ToInt32(Console.ReadLine());
+     Digit1 = Convert.ToInt32(Console.ReadLine());
 
      Console.WriteLine("Input another number and click enter");
-    num2 = Convert.ToInt32(Console.ReadLine());
+     Digit2 = Convert.ToInt32(Console.ReadLine());
 
     Console.WriteLine("Choose an option from the List below");
     Console.WriteLine("\ta - Add");
@@ -21,23 +21,23 @@
     //here we declare and tell the code what to do
     {
       case "a":
-      Console.WriteLine($"Your Result : {num1} + {num2} = " + (num1 + num2));
+      Console.WriteLine($"Your Result : {Digit1} + {Digit2} = " + (Digit1 + Digit2));
       break;
     
     case "s":
-      Console.WriteLine($"YOur Result : {num1} - {num2} = " + (num1 - num2));
+      Console.WriteLine($"YOur Result : {Digit1} - {Digit2} = " + (Digit1 - Digit2));
       break;
     
     case "m":
-      Console.WriteLine($"YOur Result : {num1} * {num2} = " + (num1 * num2));
+      Console.WriteLine($"YOur Result : {Digit1} * {Digit2} = " + (Digit1 * Digit2));
       break;
    
     case "d":
-      Console.WriteLine($"YOur Result : {num1} / {num2} = " + (num1 / num2));
+      Console.WriteLine($"YOur Result : {Digit1} / {Digit2} = " + (Digit1 / Digit2));
       break;
     }
 
-    Console.WriteLine("Press any key to exit the calculator app");
+    Console.WriteLine("Press any key to close the calculator app");
 
     Console.ReadKey();
   
